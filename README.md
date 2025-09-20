da601f174a0c66b3f8882d28edbba47fed107500
antonriabokon <195663496+antonriabokon@users.noreply.github.com>
2025-09-20 13:41:22 +0300

    version 1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+🎯 Simple Number Guessing Game
+
+Think you can outsmart the mystery number? Here’s how to play:
+
+> - The game secretly picks a number between 1 and 20 (inclusive).
+
+> - Enter your guesses—any whole number in that range is fair game.
+
+> - After each guess, you’ll learn whether your number is too high, too low, or spot on.
+
+> - You start with a set score. Each wrong guess lowers your score by 1.
+
+> - If your score drops to 0, it’s Game Over!
+
+Your mission: Guess the secret number before your score runs out. Good luck! 🏆
---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// Message function
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // No input
+  if (!guess) {
+    displayMessage('No number!');
+
+    // Win
+  } else if (guess === secretNumber) {
+    displayMessage('Correct number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+// Again btn
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+});
